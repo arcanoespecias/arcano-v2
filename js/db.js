dc911ecfc6abccc82edd2535983b619164714ea2
arcanoespecias <arcanoespecias@gmail.com>
2026-08-16 15:00:00 -0500

    Fix: imagen de especias/blends se guarda correctamente

--- js/db.js
+++ js/db.js
@@ -420,6 +420,7 @@ function saveEspecia(data) {
   data.stockGrande = Number(data.stockGrande) || 0;
   _db.especias[data.id] = data;
   _getOrCreateSticker(data.nombre);
+  if (data.imagen) writeField('especias/' + data.id + '/imagen', data.imagen);
   _saveToFirebase(); _cacheLocal();
   _notify(isNew ? 'create' : 'update', 'especias', data.id);
   return data;
@@ -465,6 +466,7 @@ function saveBlend(data) {
   data.stockGrande = Number(data.stockGrande) || 0;
   _db.blends[data.id] = data;
   _getOrCreateSticker(data.nombre);
+  if (data.imagen) writeField('blends/' + data.id + '/imagen', data.imagen);
   _saveToFirebase(); _cacheLocal();
   _notify(isNew ? 'create' : 'update', 'blends', data.id);
   return data;
